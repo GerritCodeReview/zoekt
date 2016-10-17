4d5013feaa124a510df7bab9795b5fca0d14c668
Han-Wen Nienhuys <hanwen@google.com>
2016-10-17 17:30:19 +0200

    Import termite mkbox for sandboxing.
    This makes the command-line interface independent of Bazel's
    changes. In addition, it lets us use whitelisting rather than Bazel's
    blacklisting strategy.
    
    Change-Id: I392356fcd18cf885bc88e9c505dea45841c96bae

--- build/ctags.go
+++ build/ctags.go
@@ -22,6 +22,7 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"time"
 
 	"github.com/google/zoekt"
 	"github.com/google/zoekt/ctags"
@@ -73,19 +74,18 @@ func runCTags(bin string, sandboxBin string, inputs map[string][]byte) ([]*ctags
 
 		sandboxArgs := []string{
 			sandboxBin,
-			"-t30", "-T30",
-			"-D", "-S", sandboxDir, "-M", dir, "-m", "/input", "-W", "/input",
+			"-s", sandboxDir, "-b", dir + "=input", "-d", "/input",
 			// Make sure the binary is available in the sandbox.
-			"-M", bin, "-m", "/ctags",
+			"-b", bin + "=" + "ctags", "-D", "usr",
 		}
 		args[0] = "/ctags"
-		for _, d := range []string{"/bin", "/lib", "/usr/bin", "/lib64"} {
+		for _, d := range []string{"/bin", "/lib", "/lib64", "/usr/bin"} {
 			if _, err := os.Lstat(d); err == nil {
-				sandboxArgs = append(sandboxArgs, "-M", d, "-m", d)
+				sandboxArgs = append(sandboxArgs, "-b", d+"="+d[1:])
 			}
 		}
 
-		sandboxArgs = append(sandboxArgs, "--")
+		sandboxArgs = append(sandboxArgs, "-t", "tmp", "--")
 		args = append(sandboxArgs, args...)
 	} else {
 		log.Println("WARNING: running ctags without sandboxing.")
@@ -94,15 +94,32 @@ func runCTags(bin string, sandboxBin string, inputs map[string][]byte) ([]*ctags
 	cmd := exec.Command(args[0], args[1:]...)
 	cmd.Dir = dir
 
-	var errBuf bytes.Buffer
+	var errBuf, outBuf bytes.Buffer
 	cmd.Stderr = &errBuf
-	out, err := cmd.Output()
-	if err != nil {
-		return nil, fmt.Errorf("exec(%s): %v, stderr: %s", cmd.Args, err, errBuf.String())
+	cmd.Stdout = &outBuf
+
+	if err := cmd.Start(); err != nil {
+		return nil, err
+	}
+
+	errChan := make(chan error, 1)
+	go func() {
+		err := cmd.Wait()
+		errChan <- err
+	}()
+	timeout := time.After(30 * time.Second)
+	select {
+	case <-timeout:
+		cmd.Process.Kill()
+		return nil, fmt.Errorf("timeout executing ctags.")
+	case err := <-errChan:
+		if err != nil {
+			return nil, fmt.Errorf("exec(%s): %v, stderr: %s", cmd.Args, err, errBuf.String())
+		}
 	}
 
 	var entries []*ctags.Entry
-	for _, l := range bytes.Split(out, []byte{'\n'}) {
+	for _, l := range bytes.Split(outBuf.Bytes(), []byte{'\n'}) {
 		if len(l) == 0 {
 			continue
 		}

e5add882fe82f757565edce3f2586d48fd23ea4a
Han-Wen Nienhuys <hanwen@google.com>
2017-10-25 22:06:17 +0200

    Restore host customization in search box.
    Change-Id: I9be2c8a3d58308dbfd79ece6735d6ce03b3bc0ef

--- web/templates.go
+++ web/templates.go
@@ -64,6 +64,9 @@ var TemplateText = map[string]string{
   <div class="form-group form-group-lg">
     <div class="input-group input-group-lg">
       <input class="form-control" placeholder="Search for some code..."
+              {{if .Query}}
+              value={{.Query}}
+              {{end}}
               id="searchbox" type="text" name="q">
       <div class="input-group-btn">
         <button class="btn btn-primary">Search</button>

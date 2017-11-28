81bb7afdce2ba373f055fa4cb0c926a8604cbc55
Han-Wen Nienhuys <hanwen@google.com>
2017-11-28 18:18:10 +0100

    Store github stats in git config
    These numbers can help rank search results.
    
    Change-Id: I7465f3409e1c04b78b679b2dabd823affa9fa0d2

--- cmd/zoekt-mirror-github/main.go
+++ cmd/zoekt-mirror-github/main.go
@@ -27,7 +27,10 @@ import (
 	"net/url"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
+
+	git "gopkg.in/src-d/go-git.v4"
 
 	"github.com/google/go-github/github"
 	"golang.org/x/oauth2"
@@ -113,7 +116,7 @@ func main() {
 	}
 
 	if err := cloneRepos(destDir, repos); err != nil {
-		log.Fatal(err)
+		log.Fatalf("cloneRepos: %v", err)
 	}
 
 	if *deleteRepos {
@@ -226,11 +229,65 @@ func cloneRepos(destDir string, repos []*github.Repository) error {
 			"zoekt.web-url-type": "github",
 			"zoekt.web-url":      *r.HTMLURL,
 			"zoekt.name":         filepath.Join("github.com", *r.FullName),
-			// TODO - insert stars/forks for repo ranking too.
 		}
 		if err := gitindex.CloneRepo(destDir, *r.FullName, *r.CloneURL, config); err != nil {
 			return err
 		}
+
+		if err := updateConfig(destDir, r); err != nil {
+			return fmt.Errorf("updateConfig", err)
+		}
+	}
+
+	return nil
+}
+
+func updateConfig(destDir string, r *github.Repository) error {
+	p := filepath.Join(destDir, *r.FullName+".git")
+	repo, err := git.PlainOpen(p)
+	if err != nil {
+		return fmt.Errorf("PlainOpen(%s): %v", p, err)
+	}
+
+	cfg, err := repo.Config()
+	if err != nil {
+		return err
+	}
+
+	sub := 0
+	if r.SubscribersCount != nil {
+		sub = *r.SubscribersCount
+	}
+	for k, v := range map[string]string{
+		"github-stars":       strconv.Itoa(*r.StargazersCount),
+		"github-watchers":    strconv.Itoa(*r.WatchersCount),
+		"github-subscribers": strconv.Itoa(sub),
+		"github-forks":       strconv.Itoa(*r.ForksCount),
+	} {
+		cfg.Raw.AddOption("zoekt", "", k, v)
+	}
+
+	f, err := ioutil.TempFile(p, "")
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	out, err := cfg.Marshal()
+	if err != nil {
+		return err
+	}
+
+	if _, err := f.Write(out); err != nil {
+		return err
+	}
+
+	if err := f.Close(); err != nil {
+		return err
+	}
+
+	if err := os.Rename(f.Name(), filepath.Join(p, "config")); err != nil {
+		return err
 	}
 
 	return nil

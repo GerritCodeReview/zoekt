aede5591218e6eae223ce257eaaaa0e508b6e6ac
Tobias Gruetzmacher <tobias-git@23.gs>
2018-04-17 23:02:04 +0200

    Allow U+FFFD (replacement char) in files.
    To detect real errors in utf8.DecodeRune, the returned size must be
    checked.
    
    Change-Id: I9832b4dc79a9d505189d147473b9f64b1b01f391

--- indexbuilder.go
+++ indexbuilder.go
@@ -289,7 +289,7 @@ func IsText(content []byte) bool {
 		}
 
 		r, sz := utf8.DecodeRune(content)
-		if r == utf8.RuneError {
+		if r == utf8.RuneError && sz < 2 {
 			return false
 		}
 		content = content[sz:]

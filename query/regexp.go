f84cf99837acc698e4c555e603b54bddc813e3e5
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2018-05-10 19:19:51 +0100

    query: Handle exclusion ranges in LowerRegexp
    An exclusion class is something like [^A-Z]. We need to specially handle it
    since the user intention of [^A-Z] should map to [^a-z]. If we use the
    normal mapping logic, we will do nothing since [a-z] is in [^A-Z].
    
    Unfortunetly this resulted in some pretty hairy logic. This problem maps to
    manipulating line intersections. There is possibly a way to write this in a
    clearer way, but for now we have many more unit tests.
    
    Change-Id: I157342417fa79cdb39645302718d4ae2c9a5aabf

--- query/regexp.go
+++ query/regexp.go
@@ -17,14 +17,21 @@ package query
 import (
 	"log"
 	"regexp/syntax"
+	"unicode/utf8"
 )
 
 var _ = log.Println
 
+// LowerRegexp lowers rune literals and adjusts character classes to include
+// lowercase versions of [A-Z] and [^A-Z].
+//
+// Note: We can't just use strings.ToLower since it will change the meaning of
+// regex shorthands like \S or \B.
 func LowerRegexp(r *syntax.Regexp) *syntax.Regexp {
 	newRE := *r
 	switch r.Op {
-	case syntax.OpLiteral, syntax.OpCharClass:
+	case syntax.OpLiteral:
+		// For literal strings we can simply lower each character.
 		newRE.Rune = make([]rune, len(r.Rune))
 		for i, c := range r.Rune {
 			if c >= 'A' && c <= 'Z' {
@@ -32,6 +39,20 @@ func LowerRegexp(r *syntax.Regexp) *syntax.Regexp {
 			} else {
 				newRE.Rune[i] = c
 			}
+		}
+	case syntax.OpCharClass:
+		// An exclusion class is something like [^A-Z]. We need to specially
+		// handle it since the user intention of [^A-Z] should map to
+		// [^a-z]. If we use the normal mapping logic, we will do nothing
+		// since [a-z] is in [^A-Z]. We assume we have an exclusion class if
+		// our inclusive range starts at 0 and ends at the end of the unicode
+		// range. Note this means we don't support unusual ranges like
+		// [^\x00-B] or [^B-\x{10ffff}].
+		isExclusion := len(r.Rune) >= 4 && r.Rune[0] == 0 && r.Rune[len(r.Rune)-1] == utf8.MaxRune
+		if isExclusion {
+			newRE.Rune = lowerCharClassExcludedAZ(r.Rune)
+		} else {
+			newRE.Rune = lowerCharClassAZ(r.Rune)
 		}
 	default:
 		newRE.Sub = make([]*syntax.Regexp, len(newRE.Sub))
@@ -41,6 +62,130 @@ func LowerRegexp(r *syntax.Regexp) *syntax.Regexp {
 	}
 
 	return &newRE
+}
+
+// lowerCharClassExcludedAZ returns the character class defined by r, but with
+// lowered counterparts in [A-Z] excluded in r, excluded in the result.
+//
+// eg [^B-H] will return [^B-Hb-h]
+func lowerCharClassExcludedAZ(r []rune) []rune {
+	// Algorithm:
+	// Assume r is sorted (it is!)
+	// 1. Build a list of inclusive ranges in a-z that are excluded in A-Z (excluded)
+	// 2. Copy across classes, ensuring all ranges are outside of ranges in excluded.
+	//
+	// In our comments we use the mathematical notation [x, y] and (a,
+	// b). [ and ] are range inclusive, ( and ) are range
+	// exclusive. So x is in [x, y], but not in (x, y).
+
+	// excluded is a list of _exclusive_ ranges in ['a', 'z'] that need
+	// to be removed.
+	excluded := []rune{}
+
+	// Note i starts at 1, so we are inspecting the gaps between ranges. So
+	// [r[0], r[1]] and [r[2], r[3]] implies we have an excluded range of
+	// (r[1], r[2]).
+	for i := 1; i < len(r)-1; i += 2 {
+		// (a, b) is a range that is excluded
+		a, b := r[i], r[i+1]
+		// This range doesn't exclude [A-Z], so skip (does not
+		// intersect with ['A', 'Z']).
+		if a > 'Z' || b < 'A' {
+			continue
+		}
+		// We know (a, b) intersects with ['A', 'Z']. So clamp such
+		// that we have the intersection (a, b) ^ [A, Z]
+		if a < 'A' {
+			a = 'A' - 1
+		}
+		if b > 'Z' {
+			b = 'Z' + 1
+		}
+		// (a, b) is now a range contained in ['A', 'Z'] that needs to
+		// be excluded. So we map it to the lower case version and add
+		// it to the excluded list.
+		excluded = append(excluded, a+'a'-'A', b+'b'-'B')
+	}
+
+	// Copy from re.Rune to newRE.Rune, but exclude excluded.
+	dst := make([]rune, 0, len(r))
+	for i := 0; i < len(r); i += 2 {
+		// [a, b] is a range that is included
+		a, b := r[i], r[i+1]
+
+		// Remove exclusions ranges that occur before a. They would of
+		// been previously processed.
+		for len(excluded) > 0 && a >= excluded[1] {
+			excluded = excluded[2:]
+		}
+
+		// If our exclusion range happens after b, that means we
+		// should only consider it later.
+		if len(excluded) == 0 || b <= excluded[0] {
+			dst = append(dst, a, b)
+			continue
+		}
+
+		// We now know that the current exclusion range intersects
+		// with [a, b]. Break it into two parts, the range before a
+		// and the range after b.
+		if a <= excluded[0] {
+			dst = append(dst, a, excluded[0])
+		}
+		if b >= excluded[1] {
+			dst = append(dst, excluded[1], b)
+		}
+	}
+
+	return dst
+}
+
+// lowerCharClassAZ returns the character class defined by r, but with
+// lowered counterparts of [A-Z] in r also in the result range.
+//
+// eg [B-H] will return [b-h]
+func lowerCharClassAZ(r []rune) []rune {
+	l := len(r)
+	tmp := make([]rune, len(r))
+	copy(tmp, r)
+	r = tmp
+
+	// First check if we already include [a-z]. If so we have can use the character class as is.
+	for i := 0; i < l; i += 2 {
+		if r[i] <= 'a' && r[i+1] >= 'z' {
+			return r
+		}
+	}
+	for i := 0; i < l; i += 2 {
+		a, b := r[i], r[i+1]
+		// This range doesn't include A-Z, so skip
+		if a > 'Z' || b < 'A' {
+			continue
+		}
+		simple := true
+		if a < 'A' {
+			simple = false
+			a = 'A'
+		}
+		if b > 'Z' {
+			simple = false
+			b = 'Z'
+		}
+		a, b = a+'a'-'A', b+'a'-'A'
+		if simple {
+			// The char range is within A-Z, so we can
+			// just modify it to be the equivalent in a-z.
+			r[i], r[i+1] = a, b
+		} else {
+			// The char range includes characters outside
+			// of A-Z. To be safe we just append a new
+			// lowered range which is the intersection
+			// with A-Z.
+			r = append(r, a, b)
+		}
+	}
+
+	return r
 }
 
 // RegexpToQuery tries to distill a substring search query that
--- query/regexp_test.go
+++ query/regexp_test.go
@@ -90,16 +90,61 @@ func TestRegexpParse(t *testing.T) {
 }
 
 func TestLowerRegexp(t *testing.T) {
-	in := "[a-zA-Z]fooBAR"
-	re := mustParseRE(in)
-	in = re.String()
-	got := LowerRegexp(re)
-	want := "[a-za-z]foobar"
-	if got.String() != want {
-		t.Errorf("got %s, want %s", got, want)
+	// The expected values are a bit volatile, since they come from
+	// syntex.Regexp.String. So they may change between go versions. Just
+	// ensure they make sense.
+	cases := map[string]string{
+		"[a-zA-Z]fooBAR": "[A-Za-z]foobar",
+
+		"foo":       "foo",
+		"FoO":       "foo",
+		"(?m:^foo)": "(?m:^)foo", // regex parse simplifies to this
+		"(?m:^FoO)": "(?m:^)foo",
+
+		// Ranges for the characters can be tricky. So we include many cases
+		"[A-Z]":  "[a-z]",
+		"[^A-Z]": "[^A-Za-z]",
+		"[A-M]":  "[a-m]",
+		"[^A-M]": "[^A-Ma-m]",
+		"[A]":    "a",
+		"[^A]":   "[^Aa]",
+		"[M]":    "m",
+		"[^M]":   "[^Mm]",
+		"[Z]":    "z",
+		"[^Z]":   "[^Zz]",
+		"[a-z]":  "[a-z]",
+		"[^a-z]": "[^a-z]",
+		"[a-m]":  "[a-m]",
+		"[^a-m]": "[^a-m]",
+		"[a]":    "a",
+		"[^a]":   "[^a]",
+		"[m]":    "m",
+		"[^m]":   "[^m]",
+		"[z]":    "z",
+		"[^z]":   "[^z]",
+
+		// @ is tricky since it is 1 value less than A
+		"[^A-Z@]": "[^@-Za-z]",
+
+		// full unicode range should just be a .
+		"[\\x00-\\x{10ffff}]": "(?s:.)",
+
+		"[abB-Z]":       "[b-za-b]",
+		"([abB-Z]|FoO)": "([b-za-b]|foo)",
+		`[@-\[]`:        `[@-\[a-z]`,      // original range includes A-Z but excludes a-z
+		`\S`:            `[^\t-\n\f-\r ]`, // \S is shorthand for the expected
 	}
 
-	if re.String() != in {
-		t.Errorf("got mutated original %s want %s", re.String(), in)
+	for expr, want := range cases {
+		re := mustParseRE(expr)
+		in := re.String()
+		got := LowerRegexp(re)
+		if got.String() != want {
+			t.Errorf("LowerRegexp(%q) == %q != %q", expr, got.String(), want)
+		}
+
+		if re.String() != in {
+			t.Errorf("got mutated original %s want %s", re.String(), in)
+		}
 	}
 }

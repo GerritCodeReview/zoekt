b3159a3171e4cd3cf40c452939cbea323cb995e9
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2018-05-11 22:22:54 +0100

    cmd: Add zoekt-archive-index
    This commit introduces zoekt-archive-index which can index a tarball via a
    filepath or HTTP. Many codehosts expose a way to fetch repository contents via
    an archive. One prominent example is Github's archive API. This allows one to
    index a repository without maintaining a local clone.
    
    Change-Id: I37f4f669ab9162533ffe7dea59316e71a2db3e93

---
+++ cmd/zoekt-archive-index/main.go
@@ -0,0 +1,154 @@
+// Command zoekt-archive-index indexes an archive.
+//
+// Example via github.com:
+//
+//   zoekt-archive-index -index $PWD/index -incremental -commit b57cb1605fd11ba2ecfa7f68992b4b9cc791934d -name github.com/gorilla/mux -strip_components 1 https://codeload.github.com/gorilla/mux/legacy.tar.gz/b57cb1605fd11ba2ecfa7f68992b4b9cc791934d
+package main
+
+import (
+	"errors"
+	"flag"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/url"
+	"reflect"
+	"strings"
+
+	"github.com/google/zoekt"
+	"github.com/google/zoekt/build"
+	"github.com/google/zoekt/gitindex"
+)
+
+// stripComponents removes the specified number of leading path
+// elements. Pathnames with fewer elements will return the empty string.
+func stripComponents(path string, count int) string {
+	for i := 0; path != "" && i < count; i++ {
+		i := strings.Index(path, "/")
+		if i < 0 {
+			return ""
+		}
+		path = path[i+1:]
+	}
+	return path
+}
+
+func do() error {
+	var (
+		sizeMax     = flag.Int("file_limit", 128*1024, "maximum file size")
+		shardLimit  = flag.Int("shard_limit", 100<<20, "maximum corpus size for a shard")
+		parallelism = flag.Int("parallelism", 4, "maximum number of parallel indexing processes.")
+		indexDir    = flag.String("index", build.DefaultDir, "index directory for *.zoekt files.")
+		incremental = flag.Bool("incremental", true, "only index changed repositories")
+		ctags       = flag.Bool("require_ctags", false, "If set, ctags calls must succeed.")
+
+		name   = flag.String("name", "", "The repository name for the archive")
+		urlRaw = flag.String("url", "", "The repository URL for the archive")
+		branch = flag.String("branch", "HEAD", "The branch name for the archive")
+		commit = flag.String("commit", "", "The commit sha for the archive. If incremental this will avoid updating shards already at commit")
+		strip  = flag.Int("strip_components", 0, "Remove the specified number of leading path elements. Pathnames with fewer elements will be silently skipped.")
+	)
+	flag.Parse()
+
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	opts := build.Options{
+		Parallelism:      *parallelism,
+		SizeMax:          *sizeMax,
+		ShardMax:         *shardLimit,
+		IndexDir:         *indexDir,
+		CTagsMustSucceed: *ctags,
+	}
+
+	// For now just make all these args required. In the future we can read
+	// extended attributes.
+	if *name == "" && *urlRaw == "" {
+		return errors.New("-name or -url required")
+	}
+	if *branch == "" {
+		return errors.New("-branch required")
+	}
+	if len(*commit) != 40 {
+		return errors.New("-commit required to be absolute commit sha")
+	}
+	if len(flag.Args()) != 1 {
+		return errors.New("expected argument for archive location")
+	}
+	archive := flag.Args()[0]
+
+	if *name != "" {
+		opts.RepositoryDescription.Name = *name
+	}
+	if *urlRaw != "" {
+		u, err := url.Parse(*urlRaw)
+		if err != nil {
+			return err
+		}
+		if err := gitindex.SetTemplatesFromOrigin(&opts.RepositoryDescription, u); err != nil {
+			return err
+		}
+	}
+	opts.SetDefaults()
+	opts.RepositoryDescription.Branches = []zoekt.RepositoryBranch{{Name: *branch, Version: *commit}}
+	brs := []string{*branch}
+
+	if *incremental {
+		versions := opts.IndexVersions()
+		if reflect.DeepEqual(versions, opts.RepositoryDescription.Branches) {
+			return nil
+		}
+	}
+
+	a, err := openArchive(archive)
+	if err != nil {
+		return err
+	}
+	defer a.Close()
+
+	builder, err := build.NewBuilder(opts)
+	if err != nil {
+		return err
+	}
+
+	for {
+		f, err := a.Next()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+
+		// We do not index large files
+		if f.Size > int64(opts.SizeMax) {
+			continue
+		}
+
+		contents, err := ioutil.ReadAll(f)
+		if err != nil {
+			return err
+		}
+
+		name := stripComponents(f.Name, *strip)
+		if name == "" {
+			continue
+		}
+
+		err = builder.Add(zoekt.Document{
+			Name:     name,
+			Content:  contents,
+			Branches: brs,
+		})
+		if err != nil {
+			return err
+		}
+	}
+
+	return builder.Finish()
+}
+
+func main() {
+	if err := do(); err != nil {
+		log.Fatal(err)
+	}
+}

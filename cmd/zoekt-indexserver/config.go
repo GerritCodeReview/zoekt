038247220a9fb2b13fb70469f59f95be3afc60f9
Kevni <kprettre@gmail.com>
2020-02-28 16:27:16 +0100

    Allow gerrit mirror to be defined in config
    indexserver cmd config file was not including Gerrit type
    mirror. It handles http basic auth in a CredentialPath entry
    and call zoekt-mirror-gerrit with the according flags.
    
    It also implement gerrit default weblink implementation of
    Gitiles (WebLink.Name = browse)
    
    Change-Id: Ia2fd0921ad9fc4e11914fbc61f07fd9601980492

--- cmd/zoekt-indexserver/config.go
+++ cmd/zoekt-indexserver/config.go
@@ -44,6 +44,7 @@ type ConfigEntry struct {
 	Exclude                string
 	GitLabURL              string
 	OnlyPublic             bool
+	GerritApiUrl           string
 }
 
 func randomize(entries []ConfigEntry) []ConfigEntry {
@@ -226,6 +227,19 @@ func executeMirror(cfg []ConfigEntry, repoDir string, pendingRepos chan<- string
 			if c.CredentialPath != "" {
 				cmd.Args = append(cmd.Args, "-token", c.CredentialPath)
 			}
+		} else if c.GerritApiUrl != "" {
+			cmd = exec.Command("zoekt-mirror-gerrit",
+				"-dest", repoDir)
+			if c.CredentialPath != "" {
+				cmd.Args = append(cmd.Args, "-http-credentials", c.CredentialPath)
+			}
+			if c.Name != "" {
+				cmd.Args = append(cmd.Args, "-name", c.Name)
+			}
+			if c.Exclude != "" {
+				cmd.Args = append(cmd.Args, "-exclude", c.Exclude)
+			}
+			cmd.Args = append(cmd.Args, c.GerritApiUrl)
 		}
 
 		stdout, _ := loggedRun(cmd)

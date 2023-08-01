338705f1588e3926d732930426877cab7f88e017
Dhruv Srivastava <dhruvsri@google.com>
2023-08-01 12:57:27 +0000

    Provide a Commit Message
    Change-Id: Ib75fa5e6825414d282957657b7a9924fdaf597b9
    
    Change-Id: Ib75fa5e6825414d282957657b7a9924fdaf597b9
    
    Change-Id: Ib75fa5e6825414d282957657b7a9924fdaf597b9
    
    Change-Id: Ib75fa5e6825414d282957657b7a9924fdaf597b9

--- api.go
+++ api.go
@@ -1,51 +1,6 @@
 // Copyright 2016 Google Inc. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//    http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-package zoekt
-
-import (
-	"context"
-	"fmt"
-	"time"
-
-	"github.com/google/zoekt/query"
-)
-
-// FileMatch contains all the matches within a file.
-type FileMatch struct {
-	// Ranking; the higher, the better.
-	Score float64 // TODO - hide this field?
-
-	// For debugging. Needs DebugScore set, but public so tests in
-	// other packages can print some diagnostics.
-	Debug string
-
-	FileName string
-
-	// Repository is the globally unique name of the repo of the
-	// match
-	Repository  string
-	Branches    []string
-	LineMatches []LineMatch
-
-	// Only set if requested
-	Content []byte
-
-	// Checksum of the content.
-	Checksum []byte
-
-	// Detected language of the result.
 	Language string
 
 	// SubRepositoryName is the globally unique name of the repo,
